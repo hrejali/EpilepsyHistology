e41ce13c91a46e0584ac852ddb1560148fa869cd
Hossein Rejali <hossein.rejali8@gmail.com>
2018-08-24 11:51:16 -0400

    $ git push

---
+++ code/ParameterizeStreamArea.m
@@ -0,0 +1,37 @@
+%Name: Hossein Rejali
+%Supervisor: Dr. Ali Khan
+%Date: June 15th,2018
+%Title: Resample Streamlines 
+function [Fx, Fy,List]= ParameterizeStreamArea(Streams)
+%% ............................ Description ...............................
+% ParameterizeStream(Streams)
+% Parameterize streamline to F(#,PercentageDepth)
+
+%Inputs:
+% 1) <Stream>: Merged Streamlines obtained from the correponding
+% cortical region in the Img image
+
+%Outputs:
+% 1) <BinProfile>: List of Bin Profiles.
+%% ...................... Find Length of each Streamline .................
+List=InterStreamArea(Streams);
+
+%% ............ Scatter Interp Preparation And Computation ................
+Area=[];
+StreamNum=[];NumStreams=length(Streams);
+X=[];Y=[];
+for i = 1:NumStreams-1
+ 
+    X=[X List.Stream(i).X'];
+    Y=[Y List.Stream(i).Y'];
+    Area=[Area List.Stream(i).Area'];
+    for j = 1:1/(List.StepSize)+1
+        temp(j)=i;
+    end
+    StreamNum=[StreamNum temp];
+    temp=[];
+end
+Fx=scatteredInterpolant(StreamNum(:),Area(:),X(:),'linear','none');
+Fy=scatteredInterpolant(StreamNum(:),Area(:),Y(:),'linear','none');
+
+end
