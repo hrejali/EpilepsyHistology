c2917265184670492d3a9425c8fa703ea5c76765
Hossein Rejali <hossein.rejali8@gmail.com>
2018-08-24 09:36:19 -0400

    Cortical Depth is now calculated using pathlength of streamline

--- code/CorticalDepthPer.m
+++ code/CorticalDepthPer.m
@@ -6,12 +6,15 @@ function [corticalPer]= CorticalDepthPer(Stream)
 % <corticalPer>: Vector containing how far from the cortical surface (GM)
 % the cordinate is as a percentage
 len=size(Stream);
-StartPT=norm(Stream(1,:));
-EndPT=norm(Stream(len(1),:));
-
-for i=1:len(1)
-corticalPer(i)=(norm(Stream(i,:))-StartPT)/(StartPT-EndPT);
+if(len(1)<2)
+    corticalPer(1)=0;
+    return
 end
-corticalPer=abs(corticalPer);
-
+%% ................. Pathlenth and Cortical Depth Calculation ............
+for i=1:len(1)
+%corticalPer(i)=(norm(Stream(i,:))-StartPT)/(StartPT-EndPT);
+corticalPer(i)=Pathlength(Stream(1:i,:));
+end
+%corticalPer=abs(corticalPer);
+corticalPer(:)=corticalPer(:)/corticalPer(len(1)); % divide by entire length
 end
