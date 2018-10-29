6ad1b6c249b12c4718319d1a394a1f2025ab9d2a
Hossein Rejali <hossein.rejali8@gmail.com>
2018-10-29 16:22:56 -0400

    Bug found and fixed

---
+++ code/SmoothWarp.m
@@ -0,0 +1,16 @@
+function [WarpingSmoothed]=SmoothWarp(Warping,sigma,w)
+
+sz = w;    % length of gaussFilter vector
+x = linspace(-sz / 2, sz / 2, sz);
+gaussFilter = exp(-x .^ 2 / (2 * sigma ^ 2));
+gaussFilter = gaussFilter / sum (gaussFilter); % normalize
+h = gaussFilter;
+
+for i=1:7
+    WarpingPad(:,i)=padarray(Warping(:,i,1)',[0,round(w/2)],'symmetric');
+    temp=conv(WarpingPad(:,i),h,'same');
+    WarpingSmoothed(i,:)=temp(w/2+1:length(temp)-w/2);
+end
+WarpingSmoothed=round(WarpingSmoothed');
+WarpingSmoothed(:,:,2)=Warping(:,:,2);
+end
---
+++ code/StreamFiltWM.m
@@ -0,0 +1,55 @@
+function [StreamFilt]= StreamFiltWM(Seg,Stream)
+%Name: Hossein Rejali
+%Supervisor: Dr. Ali Khan
+%Date: May 16th,2018
+%Title: Profile Filtering
+%% ............................Description................................
+% StreamFiltWM(ImgSeg,Profile)
+% Streamlines may end beyon the domain of the laplace solution (beyond GM
+% region), therefore we use the segmented image (ImgSeg) to mask out
+% regions in which the points of streamline is out of range
+% Last Step is additional for WM Streamlines!
+
+%Inputs:
+% 1) <Seg>: Segmented image assuming labels are as follows: GM==1, WM==2
+% Background==3 and Ignore==4
+% 2) <Stream>: Single Profile or Streamline obtained from the segmented image
+
+%Outputs
+% 1) <StreamFilt>: Filtered out Streamline by masking out points outside
+% of GM region
+%% ...........................Initialize Variables........................
+%check if cell array
+if(iscell(Stream))
+    Stream=cell2mat(Stream); % convert to matrix
+end
+len=size(Stream);
+%% ...............................Mask values.............................
+% Check if x y coordinates of the streamline fall outside of GM region
+for i=1:len(1)
+    vec=Stream(i,:); 
+    x=vec(1);y=vec(2);
+    if(Seg(round(y),round(x))==1)
+    %if(round(interp2(Seg,x,y,'linear'))==1)
+        StreamFilt(i,:)=Stream(i,:);
+
+    else
+        %Assumption that after the first point outside the boundary
+        % correspond to garbage data
+        break;
+    end
+end
+
+
+%% Filter points strictly extending outside the GM Region 
+% This last step is additional for White matter Streamline POITNS
+[x,y]=find(Seg==1);
+GMShape=alphaShape(x,y);
+index=GMShape.inShape(StreamFilt(:,2),StreamFilt(:,1));
+StreamFilt=[StreamFilt(index,1),StreamFilt(index,2)];
+
+sz=size(StreamFilt);
+%convert back to cell
+StreamFilt=mat2cell(StreamFilt,sz(1),sz(2));
+
+end
