3c2ea5022ebb5a722927719998c952d4e034f04b
Hossein Rejali <hossein.rejali8@gmail.com>
2018-10-29 14:07:58 -0400

    Code finds determines optimal Reference Profile which is initialized by the profile with the minimum average Euclidean distance from all other profiles.

---
+++ code/ReferenceLoc.m
@@ -0,0 +1,30 @@
+%Name: Hossein Rejali
+%Supervisor: Dr. Ali Khan
+%Date: September 7th,2018
+%Title: Reference Location
+function [Reference,Refloc,RefDist,DisMat]=ReferenceLoc(Profiles)
+%% ............................ Description ...............................
+% ReferenceLoc(Profiles)
+
+% Algoirithm will determine a reference from the set in Profiles. The
+% choice is based on a minimum average distance measure calculated from 
+% Dynamic Time Warping Algorithm. 
+
+%Inputs:
+% 1) <Profile>: Profile Set
+%Outputs:
+% 1) <Refloc>: RefLoc
+%%
+sz=size(Profiles);
+DisMat=zeros(sz(2),sz(2));
+for i=1:sz(2)
+    for j=1:sz(2)
+        %DisMat(i,j)=dtw(Profiles(:,i),Profiles(:,j));
+        DisMat(i,j)=sqrt(sum((Profiles(:,i) - Profiles(:,j)) .^ 2));
+    end
+end
+AvgDist=mean(DisMat,2);
+Refloc=find(AvgDist==min(AvgDist));
+Reference=Profiles(:,Refloc);
+RefDist=AvgDist(Refloc);
+end
