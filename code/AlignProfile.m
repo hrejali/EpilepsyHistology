f6f6320d61f906c84085a6885ce327dc033617bd
Hossein Rejali <hossein.rejali8@gmail.com>
2018-11-03 00:30:17 -0400

    Iterative warping algorithm to align profiles

--- code/AlignProfile.m
+++ code/AlignProfile.m
@@ -2,7 +2,7 @@
 %Supervisor: Dr. Ali Khan
 %Date: August 2nd,2018
 %Title: Align Profile (Additional Correction)
-function [AlignedProfile]=AlignProfile(Profiles)
+function [AlignedProfile,WarpingStruct,Refloc]=AlignProfile(Profiles)
 %% ............................ Description ...............................
 % AlignProfile(Profiles)
 % Algoirithm will locally align profiles in order to minimize distortion
@@ -10,14 +10,14 @@ function [AlignedProfile]=AlignProfile(Profiles)
 % of distortion is proportional to amount of information lost [3].The Entire set is
 % divided in a number of bins which alignment is done
 
-% Alignment method (icoshift): 
+% Alignment method (icoshift):
 %   Code: [1] https://www.mathworks.com/matlabcentral/fileexchange/29359-icoshift-interval-correlation-optimized-shifting-for-matlab-v-2014b-and-above
 %   Paper: [2] https://www.sciencedirect.com/science/article/pii/S1090780709003334
 %   Review: [3] https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3901265/
 
-% Correction Method: 
+% Correction Method:
 % Will correct or align profiles by shifting and compressing&stretching profiles
-% to a virtual reference. 
+% to a virtual reference.
 
 % Pairwise(Reference):
 % Pairwise methods require a reference profile ( spectrum ) in which other profiles
@@ -32,35 +32,42 @@ function [AlignedProfile]=AlignProfile(Profiles)
 %Outputs:
 % 1) <AlignedProfile>: Aligned Profile Set
 %% ......................... Initialization ...............................
-sz=size(Profiles);
-numBins=20;
-SmoothProfile=zeros(sz);
-%% ........................ Smooth Prolfiles .............................
-% reason for smoothing the profiles is to produce a better virtual
-% reference which is obtained from the averaged profile in a set
-for i=1:sz(2)
-    %Apply Guassian smoothing to data 
-    SmoothProfile(:,i)=smoothdata(Profiles(:,i),'gaussian');
-    
-end
+WarpingStruct=struct;
+iter=1;
+%% In Real Time Determine Reference and Peform Alignment
+%loop through every profile in blocks (#blocks=iters)
 
-%%
-%loop through every profile
 AlignedProfile=Profiles;
-for levels=10:-1:1
-    shift=0;
-    iters=sz(2)/(numBins*levels);
-    for i=0:round(iters)-1
-        Llimit=(1+i*numBins*levels)-shift;
-        Ulimit=(i+1)*numBins*levels-shift;
-        if(Ulimit>sz(2))
-            Ulimit=sz(2);
-        end
-        RefLoc=round((Ulimit-Llimit)/2);
-        AlignedProfile(:,Llimit:Ulimit)=icoshift(AlignedProfile(:,RefLoc)',AlignedProfile(:,Llimit:Ulimit)',3,'b',[0 1 1])';
-        %AlignedProfile(:,Llimit:Ulimit)=cow(AlignedProfile(:,RefLoc)',
-        shift=0.5*numBins*levels;
+%% Determine Reference
+% ReferenceLoc-Using distance metric to determine the best or most similar profile
+% within set
+[~,Refloc,~,DisMat]=ReferenceLoc(AlignedProfile);
+%Refloc=216;
+%Refloc=450;
+Ref=AlignedProfile(:,Refloc);
+iter=1;
+while(1)
+    [~,PFeat]=ExtractFeat(Ref); % Calculate Features from Previous Reference
+    %% Determine Optimal Warping using COW
+    [Warping,~,~]=cow(Ref',AlignedProfile',floor(1000/6),floor(1000/12));
+    %% Smooth Warping Path along profiles and Store Transformation 
+    SmoothWarping=SmoothWarp(Warping,50,150);
+    WarpingStruct(iter).Transforms=Warping; 
+    WarpingStruct(iter).SmoothTransforms=SmoothWarping;
+    iter=iter+1; % Record number of iterations
+    %% Apply Warping to Data Set & Determine New Reference
+    AlignedProfile=cow_apply(AlignedProfile',SmoothWarping)';
+    Ref=mean(AlignedProfile,2);
+    
+    %Determine Distance Bewteen New and Old Reference.
+    [~,Feat]=ExtractFeat(Ref);
+    distFeat=norm(Feat-PFeat);
+
+    if(distFeat<0.05 || iter==10)
+        break;
     end
 end
-
 end
+
+
+
