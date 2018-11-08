266c62127f33c7af2097e9df503c8d8a3ccd92f5
Hossein Rejali <hossein.rejali8@gmail.com>
2018-11-08 13:24:20 -0500

    Will now output Raw Profiles and Iso-area corrected Profiles and there respective foward and reverse interpolants

--- code/ProfileExtraction.m
+++ code/ProfileExtraction.m
@@ -2,7 +2,7 @@
 %Supervisor: Dr. Ali Khan
 %Date: June 15th,2018
 %Title: Bin Profile
-function [Profiles,F,FInv]= ProfileExtraction(img,Stream,interp)
+function [Output]= ProfileExtraction(img,Stream,interp)
 %% ............................ Description ...............................
 % ProfileExtraction(img,Stream,interp)
 % Bins and averages specified number of profiles  
@@ -14,16 +14,37 @@ function [Profiles,F,FInv]= ProfileExtraction(img,Stream,interp)
 % 3) <interp>: interpolation method -- Defualt: 'Linear'
 
 %Outputs:
-% 1) <Profiles>: List of Profiles.
+% 1) <Output>: struct containing profiles paramertized by depth and Area
+% and their foward and inverse interpolants.
 %% Check Interp input 
 if isempty(interp)
     interp='linear';
 end
 %% ....... Reparameterize Streamlines by Cortical Depth Percentage .............
-NumStreams=length(Stream);
-[F,FInv]=ParameterizeStreamArea(Stream); % Fx,y(streamline index, percentage depth [0 1])
+Output=struct;
+[F,FInv]=ParameterizeStream(Stream); % Fx,y(streamline index, percentage depth [0 1])
+Output.Depth.F=F;Output.Depth.FInv=FInv;
 Fx=F.Fx;Fy=F.Fy;
-%% Equally Resample along Each Streamline 
+% Equally Resample along Each Streamline
 Profiles=EquiResample(Fx,Fy,img,interp);
+Output.Depth.Profiles=Profiles;
+
+%% ....... Reparameterize Streamlines by Cortical Area Percentage .............
+[AreaInterpolant,~,DepthInterpolant]=ParameterizeStreamArea(Stream);
+
+Output.Area.F=AreaInterpolant.F;
+Output.Area.FInv=AreaInterpolant.FInv;
+
+Output.Depth.F=DepthInterpolant.F;
+Output.Depth.FInv=DepthInterpolant.FInv;
+
+%% Equally Resample along Each Streamline
+% Depth reparameterization
+F=DepthInterpolant.F;
+Output.Depth.Profiles=EquiResample(F.Fx,F.Fy,img,interp);
+
+% Area reparameterization
+F=AreaInterpolant.F;
+Output.Area.Profiles=EquiResample(F.Fx,F.Fy,img,interp);
 
 end
