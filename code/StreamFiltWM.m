df253112a98a8aa1898682293f8da7e3fc37f481
Hossein Rejali <hossein.rejali8@gmail.com>
2019-01-25 23:56:10 -0500

    Eroded GM segmentation before filtering streamline points - ensures proper merge

--- code/StreamFiltWM.m
+++ code/StreamFiltWM.m
@@ -43,7 +43,13 @@ end
 
 %% Filter points strictly extending outside the GM Region 
 % This last step is additional for White matter Streamline POITNS
-[x,y]=find(Seg==1);
+
+% Errode image before filtering to ensure proper merge
+se=[0 1 0; 1 1 1; 0 1 0];
+se=strel('arbitrary',se);
+eSeg=imerode(Seg==1,se);
+
+[x,y]=find(eSeg==1);
 GMShape=alphaShape(x,y);
 index=GMShape.inShape(StreamFilt(:,2),StreamFilt(:,1));
 StreamFilt=[StreamFilt(index,1),StreamFilt(index,2)];
