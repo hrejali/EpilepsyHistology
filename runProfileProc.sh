612f0c2ab8a256b9f650828afbe42ac9660ff398
Hossein Rejali <hrejali@gra-login3.graham.sharcnet>
2019-02-05 16:28:18 -0500

    Bash Script to run ProfileProc.m Matlab script

---
+++ runProfileProc.sh
@@ -0,0 +1,44 @@
+
+#!/bin/bash
+
+
+if [ "$#" -lt 1 ]
+then
+	echo "Usage: $0 <subjids>"
+	exit 0
+fi
+
+func_name=ProfileProc
+res=100um
+
+# Define required directories
+data_dir=~/EpilepsyQuantHistology/proc
+searchpath=./code
+
+#Load Matlab 
+module load matlab
+
+for subj in $@
+do
+	subj_dir=$data_dir/${subj}
+	list=$(ls -d $subj_dir/${res}_StreamLines/EPI*)
+
+	# run through each subject
+	for slide in $list
+	do
+		echo INPUT: $slide
+		outDir=$subj_dir/${res}_Profiles
+		echo OUTPUT DIRECTORY: $outDir
+
+		# check if directory does not exist!
+		if [ ! -e $outDir ]
+		then
+			mkdir $outDir
+		fi
+
+		# Run Matlab 
+		echo "addpath(genpath('$searchpath'));  $func_name('$slide','$outDir'); exit" | matlab -nosplash -nodesktop
+		echo ......................................... DONE ..............................................
+
+	done
+done
