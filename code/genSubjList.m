721d4207a68d50a7656223e2cdfc21db791ff4ba
Hossein Rejali <hossein.rejali8@gmail.com>
2019-02-16 22:34:09 -0500

    Re-wrote code: subject list are now read in matlab code instead of bash script

--- code/genSubjList.m
+++ code/genSubjList.m
@@ -2,7 +2,7 @@
 %Supervisor: Dr. Ali Khan
 %Date: Feb 14th,2018
 %Title: Generate Subject List
-function [subjList]= genSubjList(in_dir,out_dir)
+function [subjList]= genSubjList(in_dir,list_dir,out_dir)
 %% ............................ Description ...............................
 % genSubjList(in_dir)
 % Generates list of subjects (data) in strucutre to be later processed
@@ -13,25 +13,27 @@ function [subjList]= genSubjList(in_dir,out_dir)
 % 1) <in_dir>: Input Directory of Processed Profiles .mat files for Subject and Slide 
 
 % 2) <out_dir>: Directory to save processed .mat files
-
+%% Initialize Variables
+res='100um_5umPad';
 %% ................ Set Default Values and Check Inputs ..................
 if nargin==1 || isempty(out_dir)
     out_dir = './List'; % set output directory to the current directory
 end
 
-%% Check Output folder Exist 
-
-if ~exist([out_dir,'/subjList.mat'], 'file')
-    Index=1;
-else
-    temp=load([out_dir,'/subjList.mat']);
-    subjList=temp.subjList;
-    Index=length(subjList)+1;
+%% Load Information from List_dir
+subjList=importdata(list_dir,'\n');
+lenSubj=length(list);
+%% Load data and Save
+Index=1;
+for i = 1:lenSubj
+    subj_dir=[in_dir,'/',list(i),'/',res,'_Profiles'];
+    slideList=ls('EPI*');
+    for j=length(slideList)
+        data_dir=[subj_dir,'/',slideList(j)];
+        subjProfiles=load(data_dir);
+        dataList(Index) = subjProfiles;
+        Index=Index+1;
+    end
 end
-
-%% Load data and Save 
-subjProfiles=load(in_dir);
-subjList(Index) = subjProfiles;
-save([out_dir,'/subjList.mat'],'subjList');
-
+save([out_dir,'/subjList',num2str(index),'.mat'],'-struct','dataList');
 end
