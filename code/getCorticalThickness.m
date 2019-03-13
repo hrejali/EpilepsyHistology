3fa3ae34f55c75500a58cd9e0e46da06d4a27502
Hossein Rejali <hossein.rejali8@gmail.com>
2019-03-13 16:02:02 -0400

    Code to obtain thickness measurement for each streamline for a subject

---
+++ code/getCorticalThickness.m
@@ -0,0 +1,24 @@
+%Name: Hossein Rejali
+%Supervisor: Dr. Ali Khan
+%Date: March 13th,2019
+%Title: Get Cortical Thickness
+function [Thickness]= getCorticalThickness(Streamlines)
+%% ............................Description................................
+% getCorticalThickness(Streamlines)
+% Gets cortical thickness of each streamline 
+
+%Inputs:
+% 1) <Streamlines>: List of Streamlines
+
+%Outputs:
+% 2) <Thickness>:  List of thickness values for each Streamline
+
+[~,numStream]=size(Streamlines);
+
+%% ................. Pathlenth and Cortical Depth Calculation ............
+for i=1:numStream
+Thickness(i)=Pathlength(cell2mat(Streamlines(i)));
+end
+
+
+end
