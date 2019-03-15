f50a093dc5cbf2818c31c0476228554400e886cb
Hossein Rejali <hossein.rejali8@gmail.com>
2019-03-15 15:37:39 -0400

    smoothing is now done in getThickness.m function

--- code/tools/DispStreamlineThickness.m
+++ code/tools/DispStreamlineThickness.m
@@ -1,9 +1,13 @@
-function DispStreamlineThickness(Comp)
+function [fig]=DispStreamlineThickness(Comp)
 [~,numStream]=size(Comp.Streams);
 Thickness=getCorticalThickness(Comp(1).Streams);
-Thickness=smooth(Thickness,100);
-figure;plot(Thickness);
-figure;
+%% Plotting Thickness Measure
+% figure;subplot(1,2,1);plot(Thickness);title('Original Thickness');
+% Thickness=smooth(Thickness,100);subplot(1,2,2);
+% plot(Thickness);title('Smoothed Thickness');
+
+%% Overlay Thickness Measure on Individual Streamlines
+fig=figure;
 for Depth=0:0.01:1
     X=[];Y=[];
     for i=1:numStream
