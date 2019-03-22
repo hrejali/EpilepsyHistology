7a90857bc501ab428588d52c0820c3f371364cee
Hossein Rejali <hossein.rejali8@gmail.com>
2019-03-21 22:27:02 -0400

    Improper Reference - fixed

--- code/List2Table.m
+++ code/List2Table.m
@@ -41,11 +41,11 @@ for i=1:NumSubj
         for k=1:NumProfiles
             %Profile Features
             Density=List1(i).Comp(j).Aligned.Profiles(:,k)';
-            Area=List2(i).Comp(j).Aligned.Profiles(:,k)';
+            Area=List2(i).Data.Comp(j).Aligned.Profiles(:,k)';
             
             %Data Reduced Profile Features
             [~,DensityReduced]=ExtractFeat(List1(i).Comp(j).Aligned.Profiles(:,k));
-            [~,AreaReduced]=ExtractFeat(List2(i).Comp(j).Aligned.Profiles(:,k));
+            [~,AreaReduced]=ExtractFeat(List2(i).Data.Comp(j).Aligned.Profiles(:,k));
             
             %Streamline Features
             Curvature=List1(i).Comp(j).Curv(k);
